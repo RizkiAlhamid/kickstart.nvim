7ff978102c0e05bd50b9cf32445272ec709dbf9c
RizkiAlhamid <riskimifta@gmail.com>
2024-07-27 13:29:28 -0700

    abc

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,17 +1,4 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
-  -- Github Copilot
-  {
-    'github/copilot.vim',
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
-  },
-
   -- Gitsigns plugin for Git integration
   {
     'lewis6991/gitsigns.nvim',
